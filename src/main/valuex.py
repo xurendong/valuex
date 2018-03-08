7a8eca9d18e90f845096eaa5489a62e98411817b
xurendong <xrdlyj@163.com>
2018-03-08 11:28:40 +0800

    first commit

---
+++ src/main/valuex.py
@@ -0,0 +1,24 @@
+
+# Copyright (c) 2018-2018 the MatchX authors
+# All rights reserved.
+#
+# The project sponsor and lead author is Xu Rendong.
+# E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
+# See the contributors file for names of other contributors.
+#
+# Commercial use of this code in source and binary forms is
+# governed by a LGPL v3 license. You may get a copy from the
+# root directory. Or else you should get a specific written 
+# permission from the project author.
+#
+# Individual and educational use of this code in source and
+# binary forms is governed by a 3-clause BSD license. You may
+# get a copy from the root directory. Certainly welcome you
+# to contribute code of all sorts.
+#
+# Be sure to retain the above copyright notice and conditions.
+
+import sys
+
+if __name__ == "__main__":
+    pass
